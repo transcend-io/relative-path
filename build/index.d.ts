aab09407fb730672bed35886a777612f16e493ec
michaelfarrell76 <mkfrl09@gmail.com>
2019-07-08 12:19:45 -0700

    No build on github

--- build/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- *
- * ## RealtivePath
- * A simple class that has utilities for accessing/manipulating files from some relative path, and expected extensions.
- *
- * @module RealtivePath
- */
-// # sourceMappingURL=index.d.ts.map
